93c0c2b475cda5c3281b9038e284010c5dde7456
DmitryLenko <dias1381254@gmail.com>
2019-02-23 21:46:30 +0300

    Updates

---
+++ precache-manifest.2ae484331bf091024d95ac5b81098de7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "77cb712c5b33a19259d4",
+    "url": "/static/js/main.77cb712c.chunk.js"
+  },
+  {
+    "revision": "8ff31ac6f4915cbc1d28",
+    "url": "/static/js/1.8ff31ac6.chunk.js"
+  },
+  {
+    "revision": "77cb712c5b33a19259d4",
+    "url": "/static/css/main.3f602574.chunk.css"
+  },
+  {
+    "revision": "00a3ce74397118784f16a5ee49c3cf4c",
+    "url": "/index.html"
+  }
+];
